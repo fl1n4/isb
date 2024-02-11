53c4101f7f541e23680b7410d10444ad0b8229b9
fl1n4 <zhuravlev.serj2015@yandex.ru>
2024-02-11 15:53:13 +0500

    add_docstring_typehint

--- lab_1/encoder.py
+++ lab_1/encoder.py
@@ -1,4 +1,5 @@
-def encoder(input_file, output_file, shift):
+def encoder(input_file:str, output_file:str, shift:int):
+    '''a function for encrypting text with monoalphabetic substitution'''
     with open(input_file, 'r', encoding='utf-8') as file:
         text = file.read()
 
@@ -16,9 +17,8 @@ def encoder(input_file, output_file, shift):
         file.write(encoder_text)
 
 
-input_file = "C:/Users/zhura/Desktop/isb/lab_1/original_text.txt"
-output_file = 'output.txt'
-shift = 3
-
-
-encoder(input_file, output_file, shift)
+if __name__ == '__main__':
+    input_file = "C:/Users/zhura/Desktop/isb/lab_1/original_text.txt"
+    output_file = 'C:/Users/zhura/Desktop/isb/lab_1/output.txt'
+    shift = 3
+    encoder(input_file, output_file, shift)
