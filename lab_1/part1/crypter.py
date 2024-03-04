551a13296b76a46a2b75e5b346f6b75be5107755
fl1n4 <zhuravlev.serj2015@yandex.ru>
2024-03-04 15:23:33 +0500

    add_json_in_1part

--- lab_1/part1/crypter.py
+++ lab_1/part1/crypter.py
@@ -1,14 +1,14 @@
+import json
 import logging
 
 
 logging.basicConfig(level=logging.INFO)
 
 
-def encoder(input_file:str,
-            output_file:str,
-            shift:int) -> None:
+def encoder(input_file: str, output_file: str, shift: int) -> None:
     """
-    a function for encrypting text with monoalphabetic substitution
+    Encrypts text with monoalphabetic substitution and saves the modified text to a new file.
+    
     :param input_file: The path to the source text file.
     :param output_file: The path to the new text file where the modified text will be saved.
     :param shift: The meaning of the alphabet shift.
@@ -18,17 +18,23 @@ def encoder(input_file:str,
             text = file.read()
 
         encoder_text = ""
+        encoding_dict = {}
+
         for char in text:
             if char.isalpha():
-                if char.islower():
-                    encoder_text += chr((ord(char) - ord('а') + shift) % 32 + ord('а'))
-                else:
-                    encoder_text += chr((ord(char) - ord('А') + shift) % 32 + ord('А'))
+                base_char = 'a' if char.islower() else 'А'
+                encoded_char = chr((ord(char) - ord(base_char) + shift) % 32 + ord(base_char))
+                encoder_text += encoded_char
+                encoding_dict[char] = encoded_char
             else:
                 encoder_text += char
 
         with open(output_file, 'w', encoding='utf-8') as file:
             file.write(encoder_text)
+
+        with open('key1.json', 'w', encoding='utf-8') as json_file:
+            json.dump(encoding_dict, json_file, ensure_ascii=False)
+
     except FileNotFoundError as e:
         logging.error(f"File not found: {e.filename}")
     except Exception as e:
---
+++ lab_1/part2/decrypter.py
@@ -0,0 +1,47 @@
+import logging
+from typing import List
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+def replace_keys_with_values(json_file: str, input_file: str, output_file: str) -> None:
+    """
+    Replace keys in the input text file with their corresponding values from the JSON file
+    and write the modified text to the output file.
+    
+    json_file (str): Path to the JSON file containing key-value pairs.
+    input_file (str): Path to the input text file.
+    output_file (str): Path to the output text file.
+    """
+    try:
+        with open(json_file, 'r', encoding='utf-8') as f:
+            json_data = json.load(f)
+    except Exception as e:
+        logging.error(f"An error occurred while reading the JSON file: {e}")
+        return
+
+    try:
+        with open(input_file, 'r', encoding='utf-8') as f:
+            original_text = f.read()
+    except Exception as e:
+        logging.error(f"An error occurred while reading the input file: {e}")
+        return
+
+    for key, value in json_data.items():
+        original_text = original_text.replace(key, value)
+
+    try:
+        with open(output_file, 'w', encoding='utf-8') as f:
+            f.write(original_text)
+        logging.info(
+            "Replacement completed successfully. Results written to the output file.")
+    except Exception as e:
+        logging.error(
+            f"An error occurred while writing to the output file: {e}")
+
+
+if __name__ == '__main__':
+    path_to_input = 'C:/Users/zhura/Desktop/isb/lab_1/cod1.txt'
+    path_to_output = 'C:/Users/zhura/Desktop/isb/lab_1/decrypted_text1.txt'
+    indexes_new_letters = []
