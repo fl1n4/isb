b9c3d5a216840d3118fe18c3fe2a7b94a125d422
fl1n4 <zhuravlev.serj2015@yandex.ru>
2024-03-31 15:48:19 +0500

    add_logging

--- lab_2/task2/NIST_tests.py
+++ lab_2/task2/NIST_tests.py
@@ -1,8 +1,12 @@
+import logging
 import os
 import json
 from math import sqrt, pow
 from scipy.special import erfc
 import mpmath
+
+
+logging.basicConfig(level=logging.INFO)
 
 
 pi = {0: 0.2148, 1: 0.3672, 2: 0.2305, 3: 0.1875}
@@ -18,10 +22,13 @@ def frequency_test(bitstring:str) -> float:
     Returns:
     float: The p-value indicating the degree of agreement of the sequence with a uniform distribution.
     """
-    N = len(bitstring)
-    sum_values = sum(1 if bit == '1' else -1 for bit in bitstring)
-    p_value = erfc((sum_values) / sqrt(2*N))
-    return p_value 
+    try:
+        N = len(bitstring)
+        sum_values = sum(1 if bit == '1' else -1 for bit in bitstring)
+        p_value = erfc((sum_values) / sqrt(2*N))
+        return p_value
+    except Exception as ex:
+        logging.error(f"Error occurred during the test execution: {ex}\n")
 
 
 def runs_test(bitstring:str) -> float:
@@ -34,15 +41,18 @@ def runs_test(bitstring:str) -> float:
     Returns:
     float: The p-value indicating the degree of randomness in the sequence of runs.
     """
-    N = len(bitstring)
-    ones = bitstring.count('1')
-    E = ones / N
-    if abs(E - 0.5) >= 2 / sqrt(N):
-        return 0
-    else:
-        V = sum(int(bitstring[i] != bitstring[i+1]) for i in range(N-1))
-        p_value = erfc(abs(V-2*N*E*(1-E))/(2*sqrt(2*N)*E*(1-E)))
-        return p_value
+    try:
+        N = len(bitstring)
+        ones = bitstring.count('1')
+        E = ones / N
+        if abs(E - 0.5) >= 2 / sqrt(N):
+            return 0
+        else:
+            V = sum(int(bitstring[i] != bitstring[i+1]) for i in range(N-1))
+            p_value = erfc(abs(V-2*N*E*(1-E))/(2*sqrt(2*N)*E*(1-E)))
+            return p_value
+    except Exception as ex:
+            logging.error(f"Error occurred during the test execution: {ex.message}\n{ex.args}\n")
     
 
 def longest_run_of_ones_test(bitstring:str) -> float:
@@ -55,24 +65,27 @@ def longest_run_of_ones_test(bitstring:str) -> float:
     Returns:
     float: The p-value indicating the degree of randomness in the distribution of longest runs of ones.
     """
-    N = len(bitstring)
-    block_size=8
-    M = block_size
+    try:
+        N = len(bitstring)
+        block_size=8
+        M = block_size
 
-    max_run_lengths = [max(len(run) for run in block.split('0')) for block in [bitstring[i:i+M] for i in range(0, N, M)]]
+        max_run_lengths = [max(len(run) for run in block.split('0')) for block in [bitstring[i:i+M] for i in range(0, N, M)]]
 
-    v1 = sum(1 for length in max_run_lengths if length <= 1)
-    v2 = sum(1 for length in max_run_lengths if length == 2)
-    v3 = sum(1 for length in max_run_lengths if length == 3)
-    v4 = sum(1 for length in max_run_lengths if length >= 4)
-    V = [v1, v2, v3, v4]
+        v1 = sum(1 for length in max_run_lengths if length <= 1)
+        v2 = sum(1 for length in max_run_lengths if length == 2)
+        v3 = sum(1 for length in max_run_lengths if length == 3)
+        v4 = sum(1 for length in max_run_lengths if length >= 4)
+        V = [v1, v2, v3, v4]
 
-    x_square = 0
-    for i in range(4):
-        x_square += pow(V[i] - 16 * pi[i], 2) / (16 * pi[i])
-    p_value = mpmath.gammainc(3/2, x_square/2)
+        x_square = 0
+        for i in range(4):
+            x_square += pow(V[i] - 16 * pi[i], 2) / (16 * pi[i])
+        p_value = mpmath.gammainc(3/2, x_square/2)
 
-    return p_value
+        return p_value
+    except Exception as ex:
+            logging.error(f"Error occurred during the test execution: {ex.message}\n{ex.args}\n")
 
 
 if __name__ == "__main__":
