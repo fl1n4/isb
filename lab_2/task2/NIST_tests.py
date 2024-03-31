775a04f83c58db6ed6a33b1bcc25ec396eda620d
fl1n4 <zhuravlev.serj2015@yandex.ru>
2024-03-31 14:25:08 +0500

    test2

--- lab_2/task2/NIST_tests.py
+++ lab_2/task2/NIST_tests.py
@@ -9,3 +9,15 @@ def frequency_test(bitstring):
     sum_values = sum(1 if bit == '1' else -1 for bit in bitstring)
     p_value = erfc((sum_values) / sqrt(2*N))
     return p_value 
+
+
+def runs_test(bitstring):
+    N = len(bitstring)
+    ones = bitstring.count('1')
+    E = ones / N
+    if abs(E - 0.5) >= 2 / sqrt(N):
+        return 0  # P-значение равно нулю, если условие не выполнено
+    else:
+        V = sum(int(bitstring[i] != bitstring[i+1]) for i in range(N-1))
+        p_value = erfc(abs(V-2*N*E*(1-E))/(2*sqrt(2*N)*E*(1-E)))
+        return p_value
